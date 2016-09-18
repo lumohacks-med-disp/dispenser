3687d18d7f8ab9ae75c635aff664f0f12891ca7a
Jack Chao <eul721@gmail.com>
2016-09-18 05:19:00 +0000

    Servo code done; firebase code added)

--- servo_exp.py
+++ servo_exp.py
@@ -1,42 +1,77 @@
-from Tkinter import *
 import RPi.GPIO as GPIO
+import requests
+from firebase import firebase
 import time
+
+SERVO_PIN = 18
+LIGHT_PIN = 17
+SPEAKER_PIN = 19
+SWITCH_PIN = 23
+
+BUTTON_PRESSED = False
+
 
 # GPIO Setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-pwm = GPIO.PWM(18, 100)
-pwm.start(5)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setup(SPEAKER_PIN, GPIO.OUT)
+GPIO.setup(LIGHT_PIN, GPIO.OUT)
+GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+servoPWM = GPIO.PWM(SERVO_PIN, 50)
+speakerPWM = GPIO.PWM(SPEAKER_PIN, 100)
 
-LIGHT_OFF = 2.7
+servoPWM.start(7.5)
+speakerPWM.start(0)
 
-class App:
-
-    def __init__(self, master):
-        GPIO.output(17, False);
-        frame = Frame(master)
-        frame.pack()
-        scale = Scale(frame, from_=2, to=230,
-              orient=HORIZONTAL, command=self.update)
-        scale.grid(row=0)
+firebase = firebase.FirebaseApplication('https://lumohacks-med-disp.firebaseio.com', None)
+result = firebase.get('/patients/jeffrey_leung/alert', None)
+print(result)
 
 
-    def update(self, angle):
-        duty = float(angle) / 10.0 + 2.5
-        print duty
-        if(duty == LIGHT_OFF):
-            GPIO.output(17, False)
-        else:
-            GPIO.output(17, True)
+try:
+    while True:
+        input_state = GPIO.input(SWITCH_PIN)
+        if input_state == BUTTON_PRESSED:
+            GPIO.output(LIGHT_PIN, True)
+            servoPWM.ChangeDutyCycle(12)
+            speakerPWM.ChangeDutyCycle(24)
+            time.sleep(2)
 
-        # pwm.ChangeDutyCycle(duty)
+            speakerPWM.ChangeDutyCycle(0)
+            servoPWM.ChangeDutyCycle(2.5)
+            GPIO.output(LIGHT_PIN, False)
+            time.sleep(2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
 
+GPIO.cleanup()
 
-root = Tk()
-root.wm_title('Servo Control')
-app = App(root)
-root.geometry("200x50+0+0")
-root.mainloop()
+# class App:
+#
+#     def __init__(self, master):
+#         GPIO.output(17, False);
+#         frame = Frame(master)
+#         frame.pack()
+#         scale = Scale(frame, from_=2, to=230,
+#               orient=HORIZONTAL, command=self.update)
+#         scale.grid(row=0)
+#
+#
+#     def update(self, angle):
+#         duty = float(angle) / 10.0 + 2.5
+#         print duty
+#         if(duty == LIGHT_OFF):
+#             GPIO.output(17, False)
+#         else:
+#             GPIO.output(17, True)
+#
+#         servoPWM.ChangeDutyCycle(duty)
+#
+#
+#
+# root = Tk()
+# root.wm_title('Servo Control')
+# app = App(root)
+# root.geometry("200x50+0+0")
+# root.mainloop()
